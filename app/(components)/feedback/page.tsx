e912daf59727dc9388a54609f6181e9182d0dbfa
AHNayef <ahnayef@outlook.com>
2024-01-05 21:14:33 +0600

    Init feedback page

---
+++ app/(components)/feedback/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './feedback.module.scss'
+export default function Page() {
+  return (
+    <>
+    <div className={style.feedbackMain}>
+    
+    </div>
+    </>
+  )
+}
