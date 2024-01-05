086e065d4d5b0d405d52f8dedc7e8d39ab7bde93
AHNayef <ahnayef@outlook.com>
2024-01-05 16:23:43 +0600

    Add react-toastify package to dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.css'
 import { RiShieldStarLine } from "react-icons/ri";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+
+
+
 
 const initialData: { subList: Array<{ subName: string, grade: number, credit: number }> } = {
-  subList: [
-    {
-      subName: '',
-      grade: 0,
-      credit: 0
-    }
-  ]
+  subList: []
 }
 
 
@@ -49,68 +49,100 @@ export default function Home() {
     return <h1>{count.toFixed(2)}</h1>;
   };
 
+
+  const handleAdd = (e: any) => {
+    e.preventDefault();
+
+    if (e.target.subject.value === '') return;
+
+    // Prevent user from adding same subject twice
+    for (let i = 0; i < subState.length; i++) {
+      if (subState[i].subName === e.target.subject.value) {
+        toast.error('Subject already added');
+        return;
+      }
+    }
+
+
+    const subName = e.target.subject.value;
+    const grade = e.target[1].value;
+    const credit = e.target[2].value;
+
+    const newSub = {
+      subName: subName,
+      grade: grade,
+      credit: credit
+    }
+
+    setSubState([...subState, newSub]);
+    e.target.reset();
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.inputArea}>
-        <h1 className={styles.heading}>CGPA Calculator</h1>
-        <form className={styles.inputBox}>
+    <>
+      <ToastContainer theme='dark' />
+      <main className={styles.main}>
+        <div className={styles.inputArea}>
+          <h1 className={styles.heading}>CGPA Calculator</h1>
+          <form className={styles.inputBox} onSubmit={handleAdd}>
 
-          <div className={styles.iSub}>
-            <h3>Course: </h3>
-            <input type="text" placeholder='Course Name' id='subject' list='subjects' required />
-            <datalist id="subjects">
-              <option value="Data Structure and Algorithm" />
-              <option value="Electronic Devices and Circuits" />
-              <option value="Electronic Devices and Circuits Lab" />
-              <option value="Data Structure and Algorithm Lab" />
-              <option value="Linear Algebra" />
-              <option value="Fundamentals of physics" />
-              <option value="Advanced Functional English" />
-              <option value="Bangladesh Studies" />
-              <option value="EDC" />
-              <option value="EDC Lab" />
-              <option value="DSA" />
-              <option value="DSA Lab" />
-            </datalist>
-          </div>
-          <div className={styles.point}>
-            <div className={styles.iGrade}>
-              <h3>Grade: &nbsp;</h3>
-              <select required>
-                <option value={4}>A+</option>
-                <option value={3.75}>A</option>
-                <option value={3.5}>A-</option>
-                <option value={3.25}>B+</option>
-                <option value={3}>B</option>
-                <option value={2.75}>B-</option>
-                <option value={2.5}>C+</option>
-                <option value={2.25}>C-</option>
-                <option value={2}>D</option>
-                <option value={0}>F</option>
-              </select>
+            <div className={styles.iSub}>
+              <h3>Course: </h3>
+              <input type="text" placeholder='Course Name' id='subject' list='subjects' required />
+              <datalist id="subjects">
+                <option value="Data Structure and Algorithm" />
+                <option value="Electronic Devices and Circuits" />
+                <option value="Electronic Devices and Circuits Lab" />
+                <option value="Data Structure and Algorithm Lab" />
+                <option value="Linear Algebra" />
+                <option value="Fundamentals of physics" />
+                <option value="Advanced Functional English" />
+                <option value="Bangladesh Studies" />
+                <option value="EDC" />
+                <option value="EDC Lab" />
+                <option value="DSA" />
+                <option value="DSA Lab" />
+              </datalist>
             </div>
-            <div className={styles.iCredit}>
-              <h3>Credit: </h3>
-              <select name="" id="">
-                <option value={3}>3</option>
-                <option value={1.5}>1.5</option>
-              </select>
+            <div className={styles.point}>
+              <div className={styles.iGrade}>
+                <h3>Grade: &nbsp;</h3>
+                <select required>
+                  <option value={4}>A+</option>
+                  <option value={3.75}>A</option>
+                  <option value={3.5}>A-</option>
+                  <option value={3.25}>B+</option>
+                  <option value={3}>B</option>
+                  <option value={2.75}>B-</option>
+                  <option value={2.5}>C+</option>
+                  <option value={2.25}>C-</option>
+                  <option value={2}>D</option>
+                  <option value={0}>F</option>
+                </select>
+              </div>
+              <div className={styles.iCredit}>
+                <h3>Credit: </h3>
+                <select name="" id="">
+                  <option value={3}>3</option>
+                  <option value={1.5}>1.5</option>
+                </select>
+              </div>
             </div>
-          </div>
-          <div className={styles.add}>
-            <button type="submit">Add</button>
-          </div>
-        </form>
+            <div className={styles.add}>
+              <button type="submit">Add</button>
+            </div>
+          </form>
 
-        <div className={styles.subList}>
+          <div className={styles.subList}>
 
-          <div className={styles.sub}>
-            <h3>Subject</h3>
-            <h3>Grade</h3>
-            <h3>Credit</h3>
-          </div>
+            <div className={styles.sub}>
+              <h3>Subject</h3>
+              <h3>Grade</h3>
+              <h3>Credit</h3>
+            </div>
 
-          <div className={styles.sub}>
+            {/* <div className={styles.sub}>
             <h3>EDC</h3>
             <h3>A+</h3>
             <h3>Credit</h3>
@@ -120,31 +152,42 @@ export default function Home() {
             <h3>DSA</h3>
             <h3>A+</h3>
             <h3>Credit</h3>
+          </div>  */}
+            {subState.length > 0 &&
+              subState.map((sub, index) => {
+                return (
+                  <div className={styles.sub} key={index}>
+                    <h3>{sub.subName}</h3>
+                    <h3>{sub.grade}</h3>
+                    <h3>{sub.credit}</h3>
+                  </div>
+                )
+              })
+            }
+
+
           </div>
 
+          <div className={styles.calculate}>
+            <button>Calculate</button>
+          </div>
 
+          <div className={styles.result}>
 
-        </div>
-
-        <div className={styles.calculate}>
-          <button>Calculate</button>
-        </div>
-
-        <div className={styles.result}>
-
-          <div className={styles.cgCircle}>
-            <div className={styles.cgGradient}>
-              <div className={styles.texts}>
-                <i><RiShieldStarLine /></i>
-                <Counter end={3.65} />
-                <p>A+</p>
+            <div className={styles.cgCircle}>
+              <div className={styles.cgGradient}>
+                <div className={styles.texts}>
+                  <i><RiShieldStarLine /></i>
+                  <Counter end={3.65} />
+                  <p>A+</p>
+                </div>
               </div>
             </div>
+
           </div>
 
         </div>
-
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
